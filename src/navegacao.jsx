a517bb8a0f0c90836524914116de17aaf4b5f790
Hélder Franklin <helderfranklin80@gmail.com>
2024-01-31 23:03:27 -0300

    Main commit

---
+++ src/navegacao.jsx
@@ -0,0 +1,27 @@
+function Navegacao(props) {
+  return (
+    <div className="navegacao">
+      <input 
+        type="radio" 
+        name="opcao-pagina"
+        id="pagina-0"
+        defaultChecked
+        onClick={() => props.alterarPaginaSelecionada(0)} />
+          <label htmlFor="pagina-0">Pratos Principais</label>
+      <input 
+        type="radio" 
+        name="opcao-pagina"
+        id="pagina-1"
+        onClick={() => props.alterarPaginaSelecionada(1)} />
+          <label htmlFor="pagina-1">Sobremesas</label>
+      <input 
+        type="radio" 
+        name="opcao-pagina"
+        id="pagina-2"
+        onClick={() => props.alterarPaginaSelecionada(2)} />
+          <label htmlFor="pagina-2">Bebidas</label>
+    </div>
+  );
+}
+
+export default Navegacao;
